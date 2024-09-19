62e3b6c6d92cab52767d9b51de90436c90cafded
Mourn5367 <qwerrrt6@gmail.com>
2024-09-19 09:35:16 +0900

    클래스의 스태틱 변수를 생성자 호출을 통해 증가 해 보았다.
    클래스의 스태틱 변수는 클래스이름에 .연산자를 통해서 접근 할 수 있다.

---
+++ 20240919/src/chap8/ex8_5/Main.java
@@ -0,0 +1,13 @@
+package chap8.ex8_5;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Student park = new Student(2019122104,"Park");
+        Student kim = new Student(2019206028,"Kim");
+        Student lee = new Student(2019153237,"Lee");
+
+        System.out.printf("Stundent 객체의 수: %d", Student.count);
+    }
+}
---
+++ 20240919/src/chap8/ex8_5/Student.java
@@ -0,0 +1,16 @@
+package chap8.ex8_5;
+
+public class Student
+{
+    static int count = 0;
+
+    int id = 0;
+    String name = "";
+
+    Student(int _id, String _name)
+    {
+        id = _id;
+        name = _name;
+    }
+
+}
