f921cc7cefdf7a47a3052ee9042d1103262cc923
Mourn5367 <qwerrrt6@gmail.com>
2024-09-26 11:42:25 +0900

    랜덤 api 써보기

--- 20240926/src/chap09/ex09_3/Main.java
+++ 20240926/src/chap09/ex09_3/Main.java
@@ -8,6 +8,11 @@ public class Main
     {
         Random random = new Random();
 
-        System.out.printf("임의의 정수값: %d", random.nextInt(10));
+        System.out.printf("임의의 정수값: %d\n", random.nextInt());
+        System.out.printf("0 에서 9 사이의 임의의 정수값: %d\n", random.nextInt(10));
+        System.out.printf("1 에서 10 사이의 임의의 정수값: %d\n", random.nextInt(10)+1);
+        System.out.printf("임의의 실수값: %f\n", random.nextDouble());
+        System.out.printf("0 에서 100 사이의 임의의 실수값: %f\n", random.nextDouble(100));
+        System.out.printf("임의의 논리값: %b\n", random.nextBoolean());
     }
 }
