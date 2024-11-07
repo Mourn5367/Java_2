1d9efa9942c137ffea3029d63179cbae07ba3845
Mourn5367 <qwerrrt6@gmail.com>
2024-11-07 10:57:28 +0900

    생성자에 최상단에 this를 사용하여 기존의 생성자를 이용해 코드수를 간결하게 만들 수 있다.

--- 20241107/src/ex10_10/Item.java
+++ 20241107/src/ex10_10/Item.java
@@ -4,15 +4,38 @@ public class Item
 {
     private String name;
     private int price;
+    private int power;
 
+    public Item()
+    {
+        this("이름없는 아이템",1,10);
+        System.out.println("기본 생성자가 호출됨");
+    }
 
+    // 생성자에서 다른 생성자를 호출할 수 있다.
+    // 단 최상위 위치에서 호출해야 한다.
+    // 태어나기도 전에 행위를 한다? 말이안됨.
     // 필드명과 지역변수(매개변수)의 이름이 같을때
     // 모호함을 this 키워드로 직접 지정해주어 구분한다.
-    public Item(String name, int price)
+
+    public Item(String name)
     {
         this.name = name;
+    }
+
+    public Item(String name, int price)
+    {
+        this(name);
         this.price = price;
     }
+
+    public Item(String name, int price, int power)
+    {
+        this(name,price);
+        this.power = power;
+    }
+
+
 
     public int getPrice() {
         return price;
@@ -28,5 +51,12 @@ public class Item
 
     public void setName(String name) {
         this.name = name;
+    }
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
     }
 }
--- 20241107/src/ex10_10/MainItem.java
+++ 20241107/src/ex10_10/MainItem.java
@@ -4,10 +4,20 @@ public class MainItem
 {
     public static void main(String[] args)
     {
-        Item item = new Item("마법지팡이",888484);
+        Item item = new Item("마법지팡이",888484,10);
+        Item item1 = new Item();
 
         System.out.println("아이템명: "+ item.getName());
         System.out.println("아이템 가격: "+ item.getPrice());
 
+        System.out.println();
+
+        System.out.println("아이템명: "+ item1.getName());
+        System.out.println("아이템 가격: "+ item1.getPrice());
+
+        Item item2 = new Item("이가닌자의 검", 7777,77);
+        System.out.println("아이템명: "+ item2.getName());
+        System.out.println("아이템 가격: "+ item2.getPrice());
+        System.out.println("아이템 가격: "+ item2.getPower());
     }
 }
