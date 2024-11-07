fe0feccb40072f777dacd9d3ea3df25aaab43ac9
Mourn5367 <qwerrrt6@gmail.com>
2024-11-07 11:28:25 +0900

    주석 수정

--- 20241107/src/protected_test/Parent.java
+++ 20241107/src/protected_test/Parent.java
@@ -9,11 +9,11 @@ public class Parent
     protected void printProtected()
     {
         System.out.println("Protected 접근 지정자로 지정된 print 함수입니다.\n " +
-                "protected 접근 제한자는 패키지가 서로 다르지만 상관관계에 있는 부모의 멤버를 접근 가능");
+                "protected 접근 제한자는 패키지가 서로 다르지만 상관관계에 있는 부모의 멤버로 접근 가능");
     }
     void printDefault()
     {
         System.out.println("Default 접근 지정자로 지정된 print 함수입니다.\n" +
-                "default 접근제한자는 패키지가 서로 다르면 부모의 멤버를 접근이 불가능 하다.");
+                "default 접근제한자는 패키지가 서로 다르면 부모의 멤버로 접근이 불가능 하다.");
     }
 }
