371196993b17b8cb5ffa48a4ab279a5430213a97
Mourn5367 <qwerrrt6@gmail.com>
2024-11-07 11:46:22 +0900

    생성자 호출 순서상 자식의 생성자가 호출될때 부모의 생성자도 호출되고 실행된다. 생성자 특징상 기본생성자가 아닐경우 사용자 지정 생성자를 직접 호출하고 매개변수들을 입력해야한다. 그렇다면 부모 생성자가 기본 생성자가 아닌 상태에서 자식 객체를 생성하려고 할 때 자식 생성자에서 사용자 지정 부모 생성자를 호출하여 값을 넣어야 하고 자식 생성자에 넣고싶은 내용을 작성해야한다.

---
+++ 20241107/src/ex10_14/MainPro.java
@@ -0,0 +1,11 @@
+package ex10_14;
+
+import ex10_14.pack2.Weapon;
+
+public class MainPro
+{
+    public static void main(String[] args) {
+        Weapon weapon = new Weapon("도깨비 방망이", 77777, 777);
+        weapon.print();
+    }
+}
---
+++ 20241107/src/ex10_14/pack1/Item.java
@@ -0,0 +1,13 @@
+package ex10_14.pack1;
+
+public class Item
+{
+    protected String name;
+    protected int price;
+
+    protected Item(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+}
